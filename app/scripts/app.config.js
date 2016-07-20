6f73e3641252d0e1b82da8a49d2a7d20f6559357
alanschlindvein <alansaraujo.schlindvein@gmail.com>
2016-07-20 17:53:28 -0300

    fixed message time

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -2,7 +2,7 @@
 
 angular.module('MapApp')
 	.config(['$httpProvider', 'FlashProvider', function($httpProvider, FlashProvider) {
-		FlashProvider.setTimeout(3000000);
-		FlashProvider.setShowClose(false);
+		FlashProvider.setTimeout(5000);
+		FlashProvider.setShowClose(true);
 		$httpProvider.defaults.withCredentials = true;
 	}]);
